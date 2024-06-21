af32ba7094f4a1389ffd50f6b2f19d235f262157
Jeison <jeisonortiz1516@gmail.com>
2024-06-21 15:09:34 -0500

    format

---
+++ src/main/java/com/jeison/library/infrastructure/abstract_services/ILoanService.java
@@ -0,0 +1,10 @@
+package com.jeison.library.infrastructure.abstract_services;
+
+import com.jeison.library.api.dto.request.LoanReq;
+import com.jeison.library.api.dto.request.LoanReqToUpdate;
+import com.jeison.library.api.dto.response.LoanResp;
+import com.jeison.library.api.dto.response.LoanRespWithDetails;
+
+public interface ILoanService extends CrudService<LoanReq,LoanReqToUpdate,LoanResp,LoanRespWithDetails,Long>{
+
+}
